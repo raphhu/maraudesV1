79deaa0f180aba3003c439fbd2a9c1d0ad2c2b74
raphhu <raphaelle.humbert@essec.edu>
2017-06-14 07:59:18 +0200

    Installation de la base de données dans index.js

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
-
-var app = express();
-
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-
-app.use(express.static('client'));
+var bodyParser = require('body-parser')
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://essec:cergyisc00l@138.68.110.210:27017/admin';
 
 
-app.listen(7777, function () {
-  console.log('Example app listening on port 7777!')
-})
+
+MongoClient.connect(url, function(err, db) { 
+console.log("Connected result", err);
+
+    var maraudescontact = db.collection("maraudes-contact");
+
+    var app = express();
+
+    // parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+    app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+    app.set('view engine', 'handlebars');
+
+    app.use(express.static('client'));
+
+     app.post('/contact/new/', function (req, res) {
+        console.log(req.body);
+        maraudescontact.insert({
+            date : new Date(),
+            playerName:"Christophe"
+        }, function(){
+            res.send('created');
+        });
+    });
+
+    app.listen(7777, function () {
+      console.log('Example app listening on port 7777!')
+    });
+
+});
